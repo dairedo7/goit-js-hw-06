07f93b9845e8f351e3731b764922dac15f4b46f6
Vladyslav Svitana <vladik719@gmail.com>
2021-12-31 19:19:15 +0100

    Retrieving FormData as object in task 8

--- js/task-08.js
+++ js/task-08.js
@@ -12,8 +12,16 @@ function sendForm(evt) {
     if (email.value === "" || password.value === "") {
         return alert(`Please fill in login and password fields`);
     }
-    // let vals = Object.values(evt.currentTarget);
-    // alert(evt = { email, password, });
-    console.log(`Login: ${email.value}, password: ${password.value}`);
+
+    const formVals = {};
+    const formData = new FormData(evt.currentTarget); // Creates object FormData
+    // The FormData interface provides a way to easily construct a set of key/value pairs representing form fields and 
+    // their values, which can then be easily sent using the XMLHttpRequest.send() method
+
+    formData.forEach((elVal, prop) => {
+        formVals[prop] = elVal;
+    });
+
+    console.log("Credentials are: ", formVals);
     evt.currentTarget.reset();
 }
